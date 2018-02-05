6ee6c2ab5be5536337ac1a22335dc53a87ff0dea
Tristan Carel <tristan.carel@epfl.ch>
2018-02-05 14:23:38 +0100

    boost: 166 -> 165

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7915,7 +7915,7 @@ with pkgs;
   boost164 = callPackage ../development/libraries/boost/1.64.nix { };
   boost165 = callPackage ../development/libraries/boost/1.65.nix { };
   boost166 = callPackage ../development/libraries/boost/1.66.nix { };
-  boost16x = boost166;
+  boost16x = boost165;
   boost = boost16x;
 
   boost_process = callPackage ../development/libraries/boost-process { };
